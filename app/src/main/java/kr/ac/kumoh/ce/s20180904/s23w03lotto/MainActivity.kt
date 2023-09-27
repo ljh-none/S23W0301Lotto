0a3e5e7f7d17f800c4162784421af9244190a273
ljh09-none <ljh09210921@gmail.com>
2023-09-27 15:38:28 +0900

    ver 3

--- app/src/main/java/kr/ac/kumoh/ce/s20180904/s23w03lotto/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180904/s23w03lotto/MainActivity.kt
@@ -1,60 +1,72 @@
 package kr.ac.kumoh.ce.s20180904.s23w03lotto
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20180904.s23w03lotto.databinding.ActivityMainBinding
-import kotlin.random.Random
-import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding
+    private lateinit var model: LottoViewModel
+    private lateinit var txtNum: Array<TextView?>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.i("Lifecycle!!", "onCreate()")
+        Log.i("Lifecycle!!!", "onCreate")
+
         //setContentView(R.layout.activity_main)
-        main=ActivityMainBinding.inflate(layoutInflater)
+        main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
+        txtNum = arrayOf(main.num1, main.num2, main.num3, main.num4, main.num5, main.num6)
 
+        model = ViewModelProvider(this)[LottoViewModel::class.java]
+        //setNumbersText()
 
-        main.btnGenerate.setOnClickListener{
-            main.num1.text=Random.nextInt(1, 46).toString()
-            main.num2.text=Random.nextInt(1, 46).toString()
-            main.num3.text=Random.nextInt(1, 46).toString()
-            main.num4.text=Random.nextInt(1, 46).toString()
-            main.num5.text=Random.nextInt(1, 46).toString()
-            main.num6.text=Random.nextInt(1, 46).toString()
+        model.numbers.observe(this, Observer {
+            setNumbersText()
+        })
+
+        main.btnGenerate.setOnClickListener {
+            model.generate()
+        }
+    }
+
+    private fun setNumbersText(){
+        txtNum.forEachIndexed { index, textView ->
+            textView?.text = model.numbers.value!![index].toString()
         }
     }
 
     override fun onStart() {
         super.onStart()
-        Log.i("Lifecycle!!", "onStart()")
+        Log.i("Lifecycle!!!", "onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i("Lifecycle!!", "onResume()")
+        Log.i("Lifecycle!!!", "onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i("Lifecycle!!", "onPause()")
+        Log.i("Lifecycle!!!", "onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.i("Lifecycle!!", "onStop()")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i("Lifecycle!!", "onDestroy()")
+        Log.i("Lifecycle!!!", "onStop")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.i("Lifecycle!!", "onRestart()")
+        Log.i("Lifecycle!!!", "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle!!!", "onDestroy")
     }
 }
